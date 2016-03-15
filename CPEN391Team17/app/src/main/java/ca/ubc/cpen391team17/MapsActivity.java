7addca7a02c1167d8e7d8390d64f2deab3255c0c
Nathan Yee <ny.nathan.yee@gmail.com>
2016-03-15 16:20:03 -0700

    Perform action when FAB is pressed

--- CPEN391Team17/app/src/main/java/ca/ubc/cpen391team17/MapsActivity.java
+++ CPEN391Team17/app/src/main/java/ca/ubc/cpen391team17/MapsActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,11 +12,14 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private boolean mIsTracking;
+    private Polyline mTrackingPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Create the GPS floating action button
         FloatingActionButton fab = new FloatingActionButton(this);
         fab.show();
+        // On startup we are not tracking the GPS position of the user
+        mIsTracking = false;
+        mTrackingPath = null;
     }
 
 
@@ -50,14 +57,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
-        // Add demo PolyLine path on the map
-        mMap.addPolyline(new PolylineOptions().geodesic(true)
-                .color(Color.RED)
-                .add(new LatLng(-33.866, 151.195))  // Sydney
-                .add(new LatLng(-18.142, 178.431))  // Fiji
-                .add(new LatLng(21.291, -157.821))  // Hawaii
-                .add(new LatLng(37.423, -122.091))  // Mountain View
-        );
+    public void onFABPressed(View view) {
+        if (!mIsTracking) {
+            mIsTracking = true;
+            // Add demo PolyLine path on the map
+            mTrackingPath = mMap.addPolyline(new PolylineOptions().geodesic(true)
+                    .color(Color.RED)
+                    .add(new LatLng(-33.866, 151.195))  // Sydney
+                    .add(new LatLng(-18.142, 178.431))  // Fiji
+                    .add(new LatLng(21.291, -157.821))  // Hawaii
+                    .add(new LatLng(37.423, -122.091))  // Mountain View
+            );
+        } else {
+            mTrackingPath.remove();
+            mIsTracking = false;
+        }
     }
 }
